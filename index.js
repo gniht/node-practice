c8366aebe6b69ca7660d87557d4774580fa67c56
gniht <jasonthomasjat@gmail.com>
2022-01-10 16:29:03 -0800

    fixed html, added nav, server now serves home page only

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 
 const server = http.createServer( ( req, res ) => {
   res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>node practice</h1>');
-  res.end();     
+  
+  let url = '.' + req.url;
+  
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }    
+  });
+    
 });
 
 server.listen(3000, 'localhost', () => {
